0aba429f0f066ea069570b5d932d75c639a2b4e4
Kyle-Nadeau <78810854+Kyle-Nadeau@users.noreply.github.com>
2023-04-18 14:50:30 -0400

    Update hilbert.cpp

--- CPPFiles/hilbert.cpp
+++ CPPFiles/hilbert.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <cmath>
-
+#include <fstream>
+#include <sstream>
 
 
 // Function to generate a Hilbert curve of order n
 void hilbertCurve(int x, int y, int dir, int order) {
+    // start filestream with output file
+    std::ofstream file;
+    file.open("output.txt");
+    // begin main fcn
     if (order == 0) {
         // base case: draw a line
         std::cout << "(" << x << ", " << y << ")" << std::endl;
+        file << "F ";
     } else {
         int length = pow(2, order - 1);
         int newOrder = order - 1;
@@ -16,17 +22,21 @@ void hilbertCurve(int x, int y, int dir, int order) {
             hilbertCurve(x, y + length, 1, newOrder);
             // draw a line to the right
             hilbertCurve(x + length, y, 0, newOrder);
+            file <<  "+ ";
             // draw a line downwards
             hilbertCurve(x + length, y + length, 0, newOrder);
             // draw a line to the left
             hilbertCurve(x, y, 3, newOrder);
+            file <<  "- ";
         } else if (dir == 1) {
             // draw a line to the right
             hilbertCurve(x + length, y, 0, newOrder);
+            file <<  "+ ";
             // draw a line upwards
             hilbertCurve(x, y + length, 1, newOrder);
             // draw a line to the left
             hilbertCurve(x, y, 1, newOrder);
+            file <<  "- ";
             // draw a line downwards
             hilbertCurve(x + length, y + length, 2, newOrder);
         } else if (dir == 2) {
@@ -34,21 +44,26 @@ void hilbertCurve(int x, int y, int dir, int order) {
             hilbertCurve(x + length, y + length, 2, newOrder);
             // draw a line to the left
             hilbertCurve(x, y, 3, newOrder);
+            file <<  "- ";
             // draw a line upwards
             hilbertCurve(x, y + length, 1, newOrder);
             // draw a line to the right
             hilbertCurve(x + length, y, 2, newOrder);
+            file <<  "+ ";
         } else {
             // draw a line to the left
             hilbertCurve(x, y, 3, newOrder);
+            file <<  "- ";
             // draw a line downwards
             hilbertCurve(x + length, y + length, 0, newOrder);
             // draw a line to the right
             hilbertCurve(x + length, y, 2, newOrder);
+            file <<  "+ ";
             // draw a line upwards
             hilbertCurve(x, y + length, 3, newOrder);
         }
     }
+    file.close();
 }
 
 // main function
